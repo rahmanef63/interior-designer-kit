a124031c73eff0e55d0f22ffdd610deda63d17d1
rahmanef <rahmanef63@gmail.com>
2026-06-06 16:03:22 +0800

    Remove stray compiled convex .js; add noEmit + gitignore

--- packages/convex/convex/crons.js
+++
@@ -1,6 +0,0 @@
-import { cronJobs } from "convex/server";
-import { internal } from "./_generated/api";
-const crons = cronJobs();
-// Daily 08:00 WIB (01:00 UTC): surface at-risk deadlines.
-crons.daily("check deadlines", { hourUTC: 1, minuteUTC: 0 }, internal.automation.checkDeadlines, {});
-export default crons;
--- packages/convex/convex/members.js
+++
@@ -1,5 +0,0 @@
-import { query } from "./_generated/server";
-export const list = query({
-    args: {},
-    handler: async (ctx) => ctx.db.query("members").collect(),
-});
